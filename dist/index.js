e62011456d064f8eee2f37382d4d7cca3458b160
Vlad <vladyslav.kiskin@applicature.com>
2018-12-17 22:31:10 +0200

    minor update

--- dist/index.js
+++ dist/index.js
@@ -5,9 +5,10 @@ var config_1 = require("./config");
 var App_1 = require("./App");
 require.main.filename = 'index.js';
 var port = process.env.PORT || 3000;
+var name = process.env.NAME;
 App_1.default.listen(port, function (err) {
     if (err) {
         return config_1.logger.error(err);
     }
-    return config_1.logger.info("Server is listening on port " + port);
+    return config_1.logger.info("Hello " + name + ", server is listening on port " + port);
 });
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,11 @@ import { logger } from "./config";
 import app from './App';
 require.main.filename = 'index.js'
 const port = process.env.PORT || 3000; 
+const name = process.env.NAME
 
 app.listen(port, (err) => { 
   if (err) { 
     return logger.error(err) 
   } 
-  return logger.info(`Server is listening on port ${port}`);
+  return logger.info(`Hello ${name}, server is listening on port ${port}`);
 })
